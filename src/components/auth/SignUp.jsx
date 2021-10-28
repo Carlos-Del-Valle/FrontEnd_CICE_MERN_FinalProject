53487181b154b53dafb48cbcd841774dde449808
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-28 19:29:30 +1100

    Implement Auth signUp

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useDispatch, useSelector } from "react-redux"
+import { Redirect } from "react-router-dom"
 
 import { Typography , TextField, Button } from "@material-ui/core"
 import { makeStyles } from "@material-ui/core/styles"
+
+import { signUp } from "../../store/actions/authActions"
 
 const useStyles = makeStyles({
     formStyle: {
@@ -17,12 +21,33 @@ const useStyles = makeStyles({
 
 const SignUp= () => {
     const classes = useStyles()
+    const dispatch = useDispatch()
+    const auth = useSelector(state => state.auth)
+    const [user, setUser] = useState({
+        name: "",
+        email:"",
+        password:"",
+    })
+
+    const handleSubmit = (e) => {
+        e.preventDefault() //prevent from refreshing all the page
+        dispatch(signUp(user))
+        setUser({
+            name: "",
+            email:"",
+            password:"",
+        })
+    }
+
+    if(auth._id) return <Redirect to="/" />
+
     return (
         <>
             <form
                 className={ classes.formStyle }
                 noValidate="off"
                 autoComplete="off"
+                onSubmit={ handleSubmit }
             >
                 <Typography variant="h5"> signUp </Typography>
                 <TextField
@@ -31,6 +56,8 @@ const SignUp= () => {
                     label="enterName"
                     variant="outlined"
                     fullWidth
+                    value = { user.name }
+                    onChange ={(e) => setUser({...user, name:e.target.value})}
                 />
                 <TextField
                     className={classes.spacing}
@@ -38,6 +65,8 @@ const SignUp= () => {
                     label="enterEmail"
                     variant="outlined"
                     fullWidth
+                    value = { user.email }
+                    onChange ={(e) => setUser({...user, email:e.target.value})}
                 />
                 <TextField
                     className={classes.spacing}
@@ -46,6 +75,8 @@ const SignUp= () => {
                     label="enterPassword"
                     variant="outlined"
                     fullWidth
+                    value = { user.password }
+                    onChange ={(e) => setUser({...user, password:e.target.value})}
                 />
                 <Button
                     className={classes.spacing}
