1cde84074e3ddc99661020999b34d91a93f796d9
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-28 11:02:07 +1100

    Implement Redux

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//import { BrowserRouter as Router } from 'react-router-dom';
-//import { AuthStateProvider } from './context/auth/authState';
-//import { EntryStateProvider } from './context/entry/entryState';
+import { Provider } from "react-redux"
+import { createStore , applyMiddleware } from "redux";
+import thunk from "redux-thunk"
+
+import rootReducer from "./store/reducers/rootReducer"
+
+const store = createStore(rootReducer, applyMiddleware( thunk ))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store= { store }>
+        <App />
+
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
