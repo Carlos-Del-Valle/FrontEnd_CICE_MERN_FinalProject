ba5b4455706cfa56d3646843ba4d2e378ac07dfd
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-29 09:57:35 +1100

    Add env

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,7 @@
-export const url = "http://localhost:5000/api"
-// export const url = "https://backend-cice-mern-finalproject.herokuapp.com/api"
+export const url =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:5000/api'
+        : 'https://backend-cice-mern-finalproject.herokuapp.com/api';
 
 export const setHeaders = () => {
     const header = {
