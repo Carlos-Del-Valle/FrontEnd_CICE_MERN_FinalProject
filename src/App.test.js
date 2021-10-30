4b6a9588f76538f53886d604c50d2d772c9b1646
Carlos Del Valle <cadelvalledieguez@gmail.com>
2021-10-30 21:56:39 +1100

    Add components test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,30 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Provider } from 'react-redux';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Slacker's list/i);
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+import rootReducer from './store/reducers/rootReducer';
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+test("renders Slacker's list title", () => {
+  render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+  );
+  const linkElement = screen.getByText(/Slacker's List/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('has sign up button', () => {
+  render(
+      <Provider store={store}>
+        <App />
+      </Provider>
+  );
+
+  expect(screen.getByText(/signup/i)).toBeInTheDocument();
 });
